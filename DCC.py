469053a529e2a08dec1737ad34dddd0215d43431
Némunaire <nemunaire@pomail.fr>
2012-07-23 01:23:04 +0200

    Cleaning DCC class Accept DCC CHAT CTCP request Disconnect DCC connexions when disconnecting the server

--- DCC.py
+++ DCC.py
@@ -1,4 +1,5 @@
 import imp
+import re
 import socket
 import threading
 import time
@@ -6,6 +7,7 @@ import time
 message = __import__("message")
 imp.reload(message)
 
+#Store all used ports
 PORTS = list()
 
 class DCC(threading.Thread):
@@ -14,9 +16,8 @@ class DCC(threading.Thread):
     self.error = False
     self.stop = False
     self.stopping = threading.Event()
-    self.s = socket
-    self.connected = self.s is not None
-    self.conn = None
+    self.conn = socket
+    self.connected = self.conn is not None
     self.messages = list()
 
     self.named = dest
@@ -34,7 +35,7 @@ class DCC(threading.Thread):
     if self.port is None:
       print ("No more available slot for DCC connection")
       self.setError("Il n'y a plus de place disponible sur le serveur pour initialiser une session DCC.")
-      
+
     threading.Thread.__init__(self)
 
   def foundPort(self):
@@ -45,14 +46,6 @@ class DCC(threading.Thread):
     return None
 
   @property
-  def isDCC(self):
-    return self.DCC and not self.error
-
-  @property
-  def nick(self):
-    return self.srv.nick
-
-  @property
   def id(self):
     return self.srv.id + "/" + self.named
 
@@ -60,10 +53,66 @@ class DCC(threading.Thread):
     self.error = True
     self.srv.send_msg_usr(dest, msg)
 
-  def send_ctcp(self, to, line, cmd = None, endl = None):
-    if cmd is None: self.srv.send_ctcp(to, line)
-    elif endl is None: self.srv.send_ctcp(to, line, cmd)
-    else: self.srv.send_ctcp(to, line, cmd, endl)
+  def disconnect(self):
+    if self.connected:
+      self.stop = True
+      self.conn.shutdown(socket.SHUT_RDWR)
+      self.stopping.wait()
+      return True
+    else:
+      return False
+
+  def kill(self):
+    if self.connected:
+      self.stop = True
+      self.connected = False
+      #self.stopping.wait()#Compare with server before delete me
+      return True
+    else:
+      return False
+
+  def launch(self, mods = None):
+    if not self.connected:
+      self.stop = False
+      self.start()
+
+  def accept_user(self, host, port):
+    self.conn = socket.socket()
+    try:
+      self.conn.connect((host, port))
+      print ('Accepted user from', host, port, "for", self.named)
+      self.connected = True
+      self.stop = False
+    except:
+      self.connected = False
+      self.error = True
+      return False
+    self.start()
+    return True
+
+
+  def request_user(self, type="CHAT", filename="CHAT"):
+    #Open the port
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+      s.bind(('', self.port))
+    except:
+      try:
+        self.port = self.foundPort()
+        s.bind(('', self.port))
+      except:
+        self.setError("Une erreur s'est produite durant la tentative d'ouverture d'une session DCC.")
+        return
+    print ('Listen on', self.port, "for", self.named)
+
+    #Send CTCP request for DCC
+    self.srv.send_ctcp(self.dest, "DCC %s %s %d %d" % (type, filename, self.srv.ip, self.port), "PRIVMSG")
+
+    s.listen(1)
+    #Waiting for the client
+    (self.conn, addr) = s.accept()
+    print ('Connected by', addr)
+    self.connected = True
 
   def send_dcc(self, msg, to = None):
     if to is None or to == self.named or to == self.dest:
@@ -80,91 +129,22 @@ class DCC(threading.Thread):
     else:
       self.srv.send_dcc(msg, to)
 
-  def send_msg_final(self, channel, msg, cmd = None, endl = None):
-    if channel == self.named or channel == self.dest:
-      self.send_dcc(msg, channel)
-    else:
-      if cmd is None: self.srv.send_msg_final(to, line)
-      elif endl is None: self.srv.send_msg_final(to, line, cmd)
-      else: self.srv.send_msg_final(to, line, cmd, endl)
-
-  def send_msg_prtn(self, msg):
-    self.srv.send_msg_prtn(msg)
-
-  def send_msg_usr(self, user, msg):
-    if user == self.named or user == self.dest:
-      self.send_dcc(msg, user)
-    else:
-      self.srv.send_msg_usr(user, msg)
-
-  def send_msg (self, channel, msg, cmd = None, endl = None):
-    if cmd is None: self.srv.send_msg(channel, line)
-    elif endl is None: self.srv.send_msg(channel, line, cmd)
-    else: self.srv.send_msg(channel, line, cmd, endl)
-
-  def send_global (self, msg, cmd = None, endl = None):
-    if cmd is None: self.srv.send_global(channel, line)
-    elif endl is None: self.srv.send_global(channel, line, cmd)
-    else: self.srv.send_global(channel, line, cmd, endl)
-
-  def accepted_channel(self, chan):
-    self.srv.accepted_channel(chan)
-
-  def disconnect(self):
+  def send_file(self, filename):
+    self.request_user("FILE", os.path.basename(filename))
     if self.connected:
-      self.stop = True
-      self.s.shutdown(socket.SHUT_RDWR)
-      self.stopping.wait()
-      return True
-    else:
-      return False
+      with open(filename, 'r') as f:
+        #TODO: don't send the entire file in one packet
+        self.conn.sendall(f.read())
 
-  def kill(self):
-    if self.connected:
-      self.stop = True
+      #TODO: Waiting for response
+
+      self.conn.close()
       self.connected = False
-      #self.stopping.wait()#Compare with server before delete me
-      return True
-    else:
-      return False
-
-  def join(self, chan, password = None):
-    self.srv.join(chan, password)
-
-  def leave(self, chan):
-    self.srv.leave(chan)
-
-  def update_mods(self, mods):
-    self.srv.update_mods(mods)
-
-  def launch(self, mods = None):
-    if not self.connected:
-      self.stop = False
-      self.start()
 
   def run(self):
     self.stopping.clear()
-    #Open the port
-    self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-      self.s.bind(('', self.port))
-    except:
-      try:
-        self.port = self.foundPort()
-        self.s.bind(('', self.port))
-      except:
-        self.setError("Une erreur s'est produite durant la tentative d'ouverture d'une session DCC.")
-        return
-    print ('Listen on', self.port, "for", self.named)
-
-    #Send CTCP request for DCC
-    self.srv.send_ctcp(self.dest, "DCC CHAT CHAT 1488679878 %d" % self.port, "PRIVMSG")
-
-    self.s.listen(1)
-    #Waiting for the client
-    (self.conn, addr) = self.s.accept()
-    print ('Connected by', addr)
-    self.connected = True
+    if not self.connected:
+      self.request_user()
 
     #Start by sending all queued messages
     for mess in self.messages:
@@ -173,6 +153,8 @@ class DCC(threading.Thread):
     time.sleep(1)
 
     readbuffer = b''
+    nicksize = len(self.srv.nick)
+    Bnick = self.srv.nick.encode()
     while not self.stop:
       raw = self.conn.recv(1024) #recieve server messages
       if not raw:
@@ -182,7 +164,23 @@ class DCC(threading.Thread):
       readbuffer = temp.pop()
 
       for line in temp:
-        self.srv.treat_msg((":%s PRIVMSG %s :" % (self.named, self.srv.nick)).encode() + line, self)
+        if line[:nicksize] == Bnick and line[nicksize+1:].strip()[:10] == b'my name is':
+          name = line[nicksize+1:].strip()[11:].decode('utf-8', 'replace')
+          if re.match("^[a-zA-Z0-9_-]+$", name):
+            if name not in self.srv.dcc_clients:
+              del self.srv.dcc_clients[self.dest]
+              del self.srv.dcc_clients[self.named]
+              self.dest = name
+              self.named = self.dest + "!" + self.named.split("!")[1]
+              self.srv.dcc_clients[self.dest] = self
+              self.srv.dcc_clients[self.named] = self
+              self.send_dcc("Hi "+self.dest)
+            else:
+              self.send_dcc("This nickname is already in use, please choose another one.")
+          else:
+            self.send_dcc("The name you entered contain invalid char.")
+        else:
+          self.srv.treat_msg((":%s PRIVMSG %s :" % (self.named, self.srv.nick)).encode() + line, self.srv)
 
     if self.connected:
       self.conn.close()
