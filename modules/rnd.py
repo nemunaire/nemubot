dfa4c49d6864f4c9e0e69a061808699b21b98249
Némunaire <nemunaire@pomail.fr>
2013-01-07 12:22:07 +0100

    New module rnd to make choice

---
+++ modules/rnd.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+
+import random
+
+nemubotversion = 3.3
+
+def load(context):
+    from hooks import Hook
+    add_hook("cmd_hook", Hook(cmd_choice, "choice"))
+
+def cmd_choice(msg):
+    return Response(msg.sender, random.choice(msg.cmds[1:]), channel=msg.channel)
