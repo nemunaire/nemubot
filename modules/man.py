b0d0b4a234970e73642b390ee1f27bb52a982795
Némunaire <nemunaire@pomail.fr>
2013-01-15 11:15:25 +0100

    Man module: display whatis by default, use english location instead of user one

--- modules/man.py
+++ modules/man.py
@@ -2,12 +2,14 @@
 
 import subprocess
 import re
+import os
 
 nemubotversion = 3.3
 
 def load(context):
     from hooks import Hook
-    add_hook("cmd_hook", Hook(cmd_man, "man"))
+    add_hook("cmd_hook", Hook(cmd_man, "MAN"))
+    add_hook("cmd_hook", Hook(cmd_whatis, "man"))
 
 def help_tiny ():
     """Line inserted in the response to the command !help"""
@@ -31,11 +33,29 @@ def cmd_man(msg):
         except ValueError:
             args.append(msg.cmds[1])
 
+    os.unsetenv("LANG")
     res = Response(msg.sender, channel=msg.channel)
     with subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as proc:
         for line in proc.stdout.read().split(b"\n"):
             (line, n) = RGXP_s.subn(b'', line)
             res.append_message(line.decode())
+
+    if len(res.messages) <= 0:
+        if num is not None:
+            res.append_message("Il n'y a pas d'entrée %s dans la section %d du manuel." % (msg.cmds[1], num))
+        else:
+            res.append_message("Il n'y a pas de page de manuel pour %s." % msg.cmds[1])
+
+    return res
+
+def cmd_whatis(msg):
+    args = ["whatis", " ".join(msg.cmds[1:])]
+
+    res = Response(msg.sender, channel=msg.channel)
+    with subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as proc:
+        for line in proc.stdout.read().split(b"\n"):
+            (line, n) = RGXP_s.subn(b'', line)
+            res.append_message(" ".join(line.decode().split()))
 
     if len(res.messages) <= 0:
         if num is not None:
