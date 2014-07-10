85edb9489d04db713c866b7bb9da7dd35dcfafe7
nemunaire <nemunaire@nemunai.re>
2014-07-10 23:35:41 +0200

    Choice module: append nick to avoid response stating with a command

--- modules/rnd.py
+++ modules/rnd.py
@@ -9,4 +9,4 @@ def load(context):
     add_hook("cmd_hook", Hook(cmd_choice, "choice"))
 
 def cmd_choice(msg):
-    return Response(msg.sender, random.choice(msg.cmds[1:]), channel=msg.channel)
+    return Response(msg.sender, random.choice(msg.cmds[1:]), channel=msg.channel, nick=msg.nick)
