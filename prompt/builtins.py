b4800643e15c955644a9757a46de476e804cc49e
nemunaire <nemunaire@nemunai.re>
2014-08-29 12:03:49 +0200

    Fix list function in prompt

--- prompt/builtins.py
+++ prompt/builtins.py
@@ -41,12 +41,12 @@ def liste(toks, context, prompt):
             if l == "server" or l == "servers":
                 for srv in context.servers.keys():
                     print ("  - %s ;" % srv)
-                else:
+                if len(context.servers) == 0:
                     print ("  > No server loaded")
             elif l == "mod" or l == "mods" or l == "module" or l == "modules":
                 for mod in context.modules.keys():
                     print ("  - %s ;" % mod)
-                else:
+                if len(context.modules) == 0:
                     print ("  > No module loaded")
             elif l in prompt.HOOKS_LIST:
                 (f,d) = prompt.HOOKS_LIST[l]
