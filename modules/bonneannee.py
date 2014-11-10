4f27232fd4b2b2c76c21d8b0a1e572a498e4bb6a
nemunaire <nemunaire@nemunai.re>
2014-11-10 21:10:09 +0100

    [bonneannee] Unharcode channel to send message on 1 January

--- modules/bonneannee.py
+++ modules/bonneannee.py
@@ -14,27 +14,41 @@ from more import Response
 yr = datetime.now(timezone.utc).year
 yrn = datetime.now(timezone.utc).year + 1
 
-def load(context):
-    d = datetime(yrn, 1, 1, 0, 0, 0, 0, timezone.utc) - datetime.now(timezone.utc)
-    add_event(ModuleEvent(interval=0, offset=d.total_seconds(), call=bonneannee))
 
-def bonneannee():
-    txt = "Bonne année %d !" % yrn
-    print (txt)
-    send_response("localhost:2771", Response(txt, "#epitagueule"))
-    send_response("localhost:2771", Response(txt, "#yaka"))
-    send_response("localhost:2771", Response(txt, "#epita2014"))
-    send_response("localhost:2771", Response(txt, "#ykar"))
-    send_response("localhost:2771", Response(txt, "#42sh"))
-    send_response("localhost:2771", Response(txt, "#nemubot"))
+def load(context):
+    if not CONF or not CONF.hasNode("sayon"):
+        print("You can append in your configuration some balise to "
+              "automaticaly wish an happy new year on some channels like:\n"
+              "<sayon hostid=\"nemubot@irc.freenode.net:6667\" "
+              "channel=\"#nemutest\" />")
+
+    def bonneannee():
+        txt = "Bonne année %d !" % yrn
+        print(txt)
+        if CONF and CONF.hasNode("sayon"):
+            for sayon in CONF.getNodes("sayon"):
+                if "hostid" not in sayon or "channel" not in sayon:
+                    print("Error: missing hostif or channel")
+                    continue
+                srv = sayon["hostid"]
+                chan = sayon["channel"]
+                send_response(srv, Response(txt, chan))
+
+    d = datetime(yrn, 1, 1, 0, 0, 0, 0,
+                 timezone.utc) - datetime.now(timezone.utc)
+    add_event(ModuleEvent(interval=0, offset=d.total_seconds(),
+                          call=bonneannee))
+
 
 @hook("cmd_hook", "newyear")
 @hook("cmd_hook", str(yrn), yrn)
 def cmd_newyear(msg, yr):
-    return Response(countdown_format(datetime(yr, 1, 1, 0, 0, 1, 0, timezone.utc),
-                         "Il reste %s avant la nouvelle année.",
-                         "Nous faisons déjà la fête depuis %s !"),
+    return Response(countdown_format(datetime(yr, 1, 1, 0, 0, 1, 0,
+                                              timezone.utc),
+                                     "Il reste %s avant la nouvelle année.",
+                                     "Nous faisons déjà la fête depuis %s !"),
                     channel=msg.channel)
+
 
 @hook("cmd_rgxp", data=yrn, regexp="^[0-9]{4}$")
 def cmd_timetoyear(msg, cur):
@@ -43,7 +57,9 @@ def cmd_timetoyear(msg, cur):
     if yr == cur:
         return None
 
-    return Response(countdown_format(datetime(yr, 1, 1, 0, 0, 1, 0, timezone.utc),
-                         "Il reste %s avant %d." % ("%s", yr),
-                         "Le premier janvier %d est passé depuis %s !" % (yr, "%s")),
+    return Response(countdown_format(datetime(yr, 1, 1, 0, 0, 1, 0,
+                                              timezone.utc),
+                                     "Il reste %s avant %d." % ("%s", yr),
+                                     "Le premier janvier %d est passé "
+                                     "depuis %s !" % (yr, "%s")),
                     channel=msg.channel)
